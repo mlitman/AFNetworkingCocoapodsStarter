c63c459ec5cb56597699da97f93dc4b9be908345
Mike Litman <awesomefat@gmail.com>
2015-12-17 12:32:25 -0600

    Initial Commit

---
+++ AFNetworkingStarter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AFNetworkingStarter
+//
+//  Created by Michael Litman on 12/17/15.
+//  Copyright © 2015 thinkful. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
